1995553587f6dfc4c0d2a6fe5f11a497e056d215
Achyut <agaihr10@gmail.com>
2023-12-04 12:17:42 +0545

    Changed things by extracting bootstrap in the view section

--- Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
+++ Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e32758729f6e253970b592c1da581040a7e379")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
